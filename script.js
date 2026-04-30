4cee2b69c84f2b392e052fd9d826c6a68306acbb
purprex <gb30586@gmail.com>
2026-04-29 21:47:38 -0300

    button switch theme (light/dark mode)

--- script.js
+++ script.js
@@ -206,3 +206,10 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("change", () => {
+
+  document.body.classList.toggle("light-mode");
+
+});
